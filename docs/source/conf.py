00559c666bf2dd5310a5539b040bf55a88e226b3
Mark Sevelj <mark.sevelj@dunwright.com.au>
2021-07-27 07:50:29 +0000

    feat(sphinx):Added autodoc extension #144
    Added the autodoc extension to conf.py
    
    closes #144

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "myst_parser",
     "sphinx_copybutton",
     "sphinx_inline_tabs",
+    "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
